5721458ee41d6c393a9d0aa87be295db11283220
Oudjique <115101548+Oudjique@users.noreply.github.com>
2023-03-21 17:02:17 +0100

    Initial commit

---
+++ exo.js
@@ -0,0 +1,14 @@
+let age = 25;
+let nom = "Jean";
+let somme = 15 + 20;
+let chaine = "trop chiant purée";
+let result = chaine.toUpperCase();
+let table = ['10', '12', '14'];
+console.log(table.length);
+let objet = {
+    nom: 'Jean',
+    age: 25,
+};
+alert(objet.nom)
+let nombre = 26.3458
+nombre.toFixed(2)
